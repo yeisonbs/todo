58717684dbce9c400eea3c0f6a4c82a94dee2619
yeison bs <yeisonbs@ufps.edu.co>
2024-06-08 11:09:22 -0500

    position button align start, position header

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,31 +1,40 @@
 // src/components/Form.jsx
-import React from "react";
+
+import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 function Form({ setTodos }) {
+    const [errorMessage, setErrorMessage] = useState(""); // Estado para almacenar el mensaje de error
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const value = event.target.todo.value.trim();
         if (value.length === 0) {
-            // No agregar el item si el valor está vacío
-            alert("Please enter a valid todo item.");
+            setErrorMessage("Please enter a valid todo item."); // Establece el mensaje de error si el campo está vacío
             return;
         }
         setTodos((prevTodos) => [
             ...prevTodos,
             { title: value, id: uuidv4(), is_completed: false },]);
         event.target.reset();
+        setErrorMessage(""); // Borra el mensaje de error después de enviar el formulario con éxito
+
     };
+
     // render the form
     return (
+
         <form className="form" onSubmit={handleSubmit}>
-            <label htmlFor="todo">
+            <label htmlFor="todo"  >
                 <input
                     type="text"
                     name="todo"
                     id="todo"
                     placeholder="Write your next task"
                 />
+                {errorMessage && <span className="error-message">{errorMessage}</span>} {/* Muestra el mensaje de error si existe */}
             </label>
+
+           
             <button type="submit">
                 <span className="visually-hidden">Submit</span>
                 <svg width="24" height="24" viewBox="0 0 24 24">
@@ -37,7 +46,11 @@ function Form({ setTodos }) {
                     />
                 </svg>
             </button>
+   
+           
+            
         </form>
+
     );
 }
 export default Form;
